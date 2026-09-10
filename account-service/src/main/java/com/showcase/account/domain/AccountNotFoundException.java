4f7d6eecc0cf9ef9e871836e67e11baca267b366
walekman <pawel.walkiewicz@gmail.com>
2026-09-10 12:36:56 +0200

    feat: add AccountService orchestration layer (#5)

---
+++ account-service/src/main/java/com/showcase/account/domain/AccountNotFoundException.java
@@ -0,0 +1,10 @@
+package com.showcase.account.domain;
+
+import java.util.UUID;
+
+public class AccountNotFoundException extends RuntimeException {
+
+    public AccountNotFoundException(UUID accountId) {
+        super("Account not found: " + accountId);
+    }
+}
